021ac3965d9238bc1e786f08cc4915c5d4c707ad
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-28 00:54:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,12 +13,12 @@ require "pry"
 
 def create_project_hash
   projects = {}
- 
+
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {}
   end
- 
+
   # return the projects hash
   projects
 end
