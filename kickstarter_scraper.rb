407965c302dfd4d20005c66c3a210069f9b6b08c
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-28 00:52:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,8 @@ def create_project_hash
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").textend
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css("p.project-meta").text
 end
 
 create_project_hash
