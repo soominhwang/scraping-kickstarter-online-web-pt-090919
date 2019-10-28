39bf4c55f66ff7337270b8ed4eb74d028530ffb2
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-28 00:54:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,10 +12,16 @@ require "pry"
 
 
 def create_project_hash
-  # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+ 
+  projects = {}
+ 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+ 
+  # return the projects hash
+  projects
 end
-
-create_project_hash
