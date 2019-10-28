f48701e4e26c20325ed36e36411dd8c1423c8119
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-28 00:49:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
-  #kickstarter.css("li.project.grid_4").first
-end
+  # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").valueend
 
 create_project_hash
